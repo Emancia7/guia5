0b056767e7f81cfc5f96a553d1ad84401bbe5f65
Emancia7 <Lizzie.mancia@gmail.com>
2018-03-17 16:01:38 -0600

    Clase para calcular el area y diagonal de una figura de 4 lados.

---
+++ src/guia5/cuadrado.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia5;
+
+/**
+ *
+ * @author Emancia7
+ */
+public class cuadrado {
+
+    public double base;
+    public double altura;
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double area(){
+        return base*altura;
+    }
+    
+    public double diagonal(){
+        double numero;
+        numero = Math.pow(base, 2)+Math.pow(altura, 2);
+        return Math.pow(numero, 0.5);
+    }
+    
+}
